8eceed9eda054d71ea9ee78e4c927338fc61db79
Sam Leonard <sam.leonard@embecosm.com>
2019-07-15 16:29:24 +0100

    Minor refactor.

--- final.py
+++ final.py
@@ -1,100 +1,108 @@
-#!/usr/bin/python3
-import time as wait
-import sounddevice as sd
-import numpy as np
-import matplotlib.pyplot as plt
+from csv import writer
 from datetime import datetime
-import pandas as pd
-import sys
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.utils import formatdate
+from sys import argv
+import matplotlib.pyplot as plt
+import numpy as np
+import smtplib
+import sounddevice as sd
+import time
 
-fs = 44100
-plotx = []
-ploty = []
 
-datestring = datetime.strftime(datetime.now(), '%d/%m/%Y')
-timestring = wait.strftime('%H:%M:%S')
+SAMPLERATE = 44100
 
-print (datestring)
-print (timestring)
-print (" ")
-def point(seconds):
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
-    sd.wait()
+
+def sound_max(seconds):
+    """
+    This function generates data points for the graph.
+    It returns the maximum value of the sound-data which
+    is recorded in the ten second period following the
+    function call.
+    """
+    myrecording = sd.rec(seconds * SAMPLERATE, samplerate=SAMPLERATE, channels=1)
+    sd.wait(seconds)
     mx = np.max(myrecording)
     return mx
 
-print ("Start of day")
-print (" ")
-endh = int(sys.argv[1])
-endm = int(sys.argv[2])
 
-while datetime.now().hour < endh or datetime.now().minute < endm:
-    # Get sound level
-    a = point(10)
-    # Compute x as time as fractions of an hour
-    n = datetime.now ()
-    h = n.hour
-    m = n.minute
-    s = n.second
-    t = h + m / 60.0 + s / 3600.0
-    plotx.append (t)
-    ploty.append (a)
-    print (t, a)
-    
-plt.plot (plotx, ploty)
-plt.xlabel ('time')
-plt.ylabel ('max points')
-plt.ylim (0, 1)
+# parse command line arguments
+if len(argv) < 2 or len(argv) > 3:  # print usage
+    print(f"Usage: {argv[0]} <end hour> <end minute>")
+    print("specifying end minute is optional however end hour must be specified")
+    exit(1)
+elif len(argv) == 2:  # only hour specified
+    end_hour, end_minute = int(argv[1]), 0
+else:  # hour and minute specified
+    end_hour, end_minute = map(int, argv[1:3])
+
+
+# gather data
+xs, ys = [], []
+
+print(f"Gathering data until {end_hour:02d}:{end_minute:02d}")
+
+while datetime.now().hour < end_hour or datetime.now().minute < end_minute:
+    # Get maximum sound in 10 seconds
+    data_point = sound_max(10)
+    # Compute t as time as fractions of an hour
+    now = datetime.now()
+    t = now.hour + (now.minute / 60) + (now.second / 3600)
+    xs.append(t)
+    ys.append(data_point)
+    print(f"{t}: {data_point}")
+
+
+# graph data and save to a PNG
+print("graphing data")
+
+plt.plot(xs, ys)
+plt.xlabel("time")
+plt.ylabel("max points")
+plt.ylim(0, 1)
 plt.grid(True)
 
-plt.savefig("graph.png")
+plt.savefig("/tmp/graph.png")
 
-import csv
-with open('data.csv', 'w', newline='') as f:
-    datawriter = csv.writer(f)
-    data = zip(["Time"] + plotx, ["Noise"] + ploty)
+
+# save data to a csv file
+print("saving raw data to a CSV")
+with open("/tmp/data.csv", "w", newline="") as f:
+    datawriter = writer(f)
+    data = zip(["Time"] + xs, ["Noise"] + ys)
     datawriter.writerows(data)
 
-print (" ")
-print ("End of day")
 
-wait.sleep(5)
-
-import smtplib
-from os.path import basename
-from email.mime.application import MIMEApplication
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate
-
-s = smtplib.SMTP('smtp.gmail.com', 587)
+# send email to google group
+print("Sending email to google group")
+s = smtplib.SMTP("smtp.gmail.com", 587)
 s.starttls()
 s.ehlo()
 
-username='noisemonitor123@gmail.com'
-password='NoiseMonitor123'
-s.login(username,password)
+username = "noisemonitor123@gmail.com"
+password = "NoiseMonitor123"
+s.login(username, password)
 
-sendto=['noisemonitor@googlegroups.com']
+
+sendto = ["noisemonitor@googlegroups.com"]
 
 msg = MIMEMultipart()
 msg["From"] = username
 msg["To"] = sendto[0]
 msg["Date"] = formatdate(localtime=True)
-msg["Subject"] = f"Graph for {datestring}"
 
-files = ["/home/pi/Documents/Noise-Monitor/graph.png", "/home/pi/Documents/Noise-Monitor/data.csv"]
+current_date = datetime.strftime(datetime.now(), "%d/%m/%Y")
+msg["Subject"] = f"Graph for {current_date}"
+
+# add attachments
+files = ["/tmp/graph.png", "/tmp/data.csv"]
 for f in files:
-    with open(f, "rb") as fil:
-        part = MIMEApplication(
-            fil.read(),
-            Name=basename(f)
-        )
-    # After the file is closed
-    part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+    with open(f, "rb") as attachment:
+        part = MIMEApplication(attachment.read(), Name=f)
+
+    part["Content-Disposition"] = f"attachment; filename={f}"
     msg.attach(part)
 
 s.sendmail(username, sendto, msg.as_string())
-
-rslt=s.quit()
-
+s.quit()
